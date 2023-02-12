26c3c680b80bff6175af3cc6ab6d17a33afb5860
mrblob <14hattoj@gmail.com>
2023-02-12 19:19:17 +0000

    Implemented the two other amplifier types.

--- main.c
+++ main.c
@@ -1,18 +1,200 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* If
- *
- */
-void summingAmplifier(int vMinus, int vPlus);
+void summing_amplifier(int vMinus, int vPlus);
 
-void invertingAmplifier(int vMinus, int vPlus);
+void inverting_amplifier(int vMinus, int vPlus);
 
-void nonInvertingAmplifier(int vMinus, int vPlus);
+void non_inverting_amplifier(int vMinus, int vPlus);
 
-void printQuestions(float variable[], char *question[], int size, bool require_positive);
+float print_questions(float variables_in[], char *question[], int size, bool require_positive);
 
 void print_vOut(float vOut, int vPlus, int vMinus);
+
+void gain(float resistors[], int length, bool is_invert);
+
+void clear_buffer(void);
+
+int check_real_int(char *question);
+
+float int_range(char *question, float min, float max);
+
+bool yes_no(char input);
+
+int main(void) {
+    bool repeat; // True for repeating the program again.
+    bool repeat_a; // True for repeating the function with the same amplifier.
+    int amp_op = 0; // Used to choose the amplifier type used.
+
+    do {
+        char repeat_q;
+        char repeat_amp;
+
+        if (amp_op == 0) {
+            amp_op = int_range("What type of amp would you like to calculate?\n"
+                               "1. Summing Amplifier\n"
+                               "2. Inverting Amplifier\n"
+                               "3. Non-Inverting Amplifier\n", 1, 3);
+
+            printf("Option: %i selected.\n\n", amp_op);
+        }
+
+
+        float vPlus = int_range("What value is needed for the positive voltage rail of the op amp?\n", 0, 600);
+        // This limits the positive rail of the amplifier to 600V max.
+
+        float vMinus = int_range("What value is needed for the negative voltage rail of the op amp?\n", -600, 0);
+
+        if (amp_op == 1) {
+            summing_amplifier(vMinus, vPlus);
+        } else if (amp_op == 2) {
+            inverting_amplifier(vMinus, vPlus);
+        } else if (amp_op == 3) {
+            non_inverting_amplifier(vMinus, vPlus);
+        }
+
+        clear_buffer();
+        printf("\nDo you want to reset the values and try again? (Y/N)\n");
+        scanf("%c", &repeat_q);
+        repeat = yes_no(repeat_q);
+
+        if (repeat == true) {
+            clear_buffer();
+            printf("\nDo you want to use the same amplifier again? (Y/N)\n");
+            scanf("%c", &repeat_amp);
+            repeat_a = yes_no(repeat_amp);
+            if (repeat_a == false) {
+                amp_op = 0;
+            }
+        }
+
+    } while (repeat == true);
+
+    return 0;
+}
+
+
+void summing_amplifier(int vMinus, int vPlus) {
+    float resistors[3];
+    float voltages[2];
+    float vOut;
+
+    char *resistor_questions[] = {
+            "What is the value for R1?\n",
+            "What is the value for R2?\n",
+            "What is the value for Rf?\n"
+    };
+
+    char *voltage_questions[] = {
+            "What is the value for V1?\n",
+            "What is the value for V2?\n"
+    };
+
+
+    print_questions(resistors, resistor_questions, 3, true);
+    print_questions(voltages, voltage_questions, 2, false);
+
+    vOut = (-resistors[2] *
+            ((voltages[0] / resistors[0]) + (voltages[1] / resistors[1])));
+
+    print_vOut(vOut, vPlus, vMinus);
+
+    gain(resistors, 2, true);
+
+}
+
+void inverting_amplifier(int vMinus, int vPlus) {
+    float resistors[2];
+    float voltages[1];
+    float vOut;
+
+    char *resistor_questions[] = {
+            "What is the value for R1?\n",
+            "What is the value for Rf?\n"
+    };
+
+    char *voltage_questions[] = {
+            "What is the value for Vin?\n"
+    };
+
+    print_questions(resistors, resistor_questions, 2, true);
+    print_questions(voltages, voltage_questions, 1, false);
+
+    vOut = (-(resistors[1] / resistors[0]) * voltages[0]);
+
+    print_vOut(vOut, vPlus, vMinus);
+
+    gain(resistors, 1, true);
+}
+
+
+void non_inverting_amplifier(int vMinus, int vPlus) {
+    float resistors[2];
+    float voltages[1];
+    float vOut;
+
+    char *resistor_questions[] = {
+            "What is the value for R1?\n",
+            "What is the value for Rf?\n"
+    };
+
+    char *voltage_questions[] = {
+            "What is the value for Vin?\n"
+    };
+
+    print_questions(resistors, resistor_questions, 2, true);
+    print_questions(voltages, voltage_questions, 1, false);
+
+    vOut = (voltages[0] + (voltages[0] / resistors[0]) * resistors[1]);
+
+    print_vOut(vOut, vPlus, vMinus);
+
+    gain(resistors, 1, false);
+
+}
+
+float print_questions(float variables[], char *question[], int size, bool require_positive) {
+    for (int i = 0; i < size; i++) {
+        float value = 0;
+        if (require_positive == true) {
+            while (value <= 0) {
+                value = check_real_int(question[i]);
+                if (value <= 0) {
+                    printf("The value entered is not a positive value, please try again.\n");
+                }
+            }
+        } else if (require_positive == false) {
+            value = check_real_int(question[i]);
+        }
+        variables[i] = value;
+    }
+}
+
+void print_vOut(float vOut, int vPlus, int vMinus) {
+    if (vOut > vPlus) {
+        vOut = vPlus;
+        printf("\nThe voltage is more than the positive rail! The voltage rail that has been exceeded is: %iV\n",
+               vPlus);
+    } else if (vOut < vMinus) {
+        vOut = vMinus;
+        printf("\nThe voltage is more than the negative rail! The voltage rail that has been exceeded is: %iV\n",
+               vMinus);
+    }
+
+    printf("The voltage output of the  amplifier is: %.2fV\n", vOut);
+}
+
+void gain(float resistors[], int length, bool is_invert) {
+    float gain;
+    for (int i = 0; i < length; i++) {
+        if (is_invert == true) {
+            gain = (-(resistors[length] / resistors[i]));
+        } else if (is_invert == false) {
+            gain = (1 + (resistors[1] / resistors[0]));
+        }
+        printf("\nThe gain for input %i is: %.2f.", i + 1, gain);
+    }
+}
 
 void clear_buffer(void) {
     // This function clears whatever# is left from the input.
@@ -23,7 +205,6 @@ void clear_buffer(void) {
     } while (clear != EOF && clear != '\n');
     // This while loop will repeat until the last thing in the buffer is a new line, which is given once the user presses enter to submit an input.
 }
-
 
 int check_real_int(char *question) {
     // This function checks if the value input is actually an integer, this prevents words being input, for example.
@@ -49,134 +230,14 @@ float int_range(char *question, float min, float max) {
     return result;
 }
 
-int main(void) {
-    bool repeat;
-    int amp_op = 0;
-    do {
-        char repeat_q;
-        if(amp_op == 0) {
-            amp_op = int_range("What type of amp would you like to calculate?\n"
-                               "1. Summing Amplifier\n"
-                               "2. Inverting Amplifier\n"
-                               "3. Non-Inverting Amplifier\n", 1, 3);
+bool yes_no(char input) {
+    if (input == 'Y' || input == 'y') {
+        return true;
 
-            printf("Option: %i selected.\n\n", amp_op);
-        }
+    } else if (input == 'N' || input == 'n') {
+        return false;
 
-        float vPlus = int_range("What value is needed for the positive voltage rail of the op amp?\n", 0, 600);
-        // This limits the positive rail of the amplifier to 600V max.
-
-        float vMinus = int_range("What value is needed for the negative voltage rail of the op amp?\n", -600, 0);
-
-        if (amp_op == 1) {
-            summingAmplifier(vMinus, vPlus);
-        } else if (amp_op == 2) {
-            invertingAmplifier(vMinus, vPlus);
-        } else if (amp_op == 3) {
-            nonInvertingAmplifier(vMinus, vPlus);
-        }
-
-        clear_buffer();
-        printf("\nDo you want to reset the values and try again? (Y/N)\n");
-        scanf("%c", &repeat_q);
-
-        if (repeat_q == 'Y' || repeat_q == 'y') {
-            repeat = true;
-        } else if (repeat_q == 'N' || repeat_q == 'n') {
-            repeat = false;
-        } else {
-            printf("This is not a valid input. Use Y or N.\n");
-        }
-    } while (repeat == true);
-
-
-    return 0;
-}
-
-void summingAmplifier(int vMinus, int vPlus) {
-    float resistors[3];
-    float voltages[2];
-    float vOut;
-
-    char *resistor_questions[] = {
-            "What is the value for R1?\n",
-            "What is the value for R2?\n",
-            "What is the value for Rf?\n"
-    };
-    char *voltage_questions[] = {
-            "What is the value for V1?\n",
-            "What is the value for V2?\n"
-    };
-
-    printQuestions(resistors, resistor_questions, 3, true);
-    printQuestions(voltages, voltage_questions, 2, false);
-
-    vOut = (-resistors[2] *
-            ((voltages[0] / resistors[0]) + (voltages[1] / resistors[1])));
-
-    print_vOut(vOut, vPlus, vMinus);
-}
-
-void invertingAmplifier(int vMinus, int vPlus) {
-
-}
-
-void nonInvertingAmplifier(int vMinus, int vPlus) {
-    /*float resistors[3];
-    float voltages[2];
-    float vOut;
-
-    char resistor_questions[3][30] = {
-            "What is the value for R1?\n",
-            "What is the value for R2?\n",
-            "What is the value for Rf?\n"
-    };
-    char voltage_questions[2][30] = {
-            "What is the value for V1?\n",
-            "What is the value for V2?\n"
-    };
-
-    printQuestions(resistors, resistor_questions, 3, 1);
-    printQuestions(voltages, voltage_questions, 2, 0);
-
-    vOut = (-resistors[2] *
-            ((voltages[0] / resistors[0]) + (voltages[1] / resistors[1])));
-
-    print_vOut(vOut, vPlus, vMinus);*/
-}
-
-void printQuestions(float variable[], char *question[], int size, bool require_positive) {
-    for (int i = 0; i < size; i++) {
-        float value = 0;
-        if (require_positive == true) {
-            while (value <= 0) {
-                value = check_real_int(question[i]);
-                if (value <= 0) {
-                    printf("The value entered is not a positive value, please try again.\n");
-                }
-            }
-        } else if (require_positive == false) {
-            value = check_real_int(question[i]);
-        }
-        variable[i] = value;
-
+    } else {
+        printf("This is not a valid input. Use Y or N.\n");
     }
-}
-
-void print_vOut(float vOut, int vPlus, int vMinus) {
-    vPlus = 12;
-    vMinus = -12;
-
-    if (vOut > vPlus) {
-        vOut = vPlus;
-        printf("The voltage is more than the positive rail! The voltage rail that has been exceeded is: %iV\n",
-               vPlus);
-    } else if (vOut < vMinus) {
-        vOut = vMinus;
-        printf("The voltage is more than the negative rail! The voltage rail that has been exceeded is: %iV\n",
-               vMinus);
-    }
-
-    fflush(stdout);
-    printf("The voltage output of the  amplifier is: %.2fV\n", vOut);
 }
